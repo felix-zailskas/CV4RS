2c8e500f2bd200026006efbd2e09e3781f9691dd
s47v <sebastian.voelkers@campus.tu-berlin.de>
2023-12-09 19:40:01 +0100

    Added MLP-Mixer

---
+++ models/MLPMixer.py
@@ -0,0 +1,6 @@
+import timm
+
+
+def create_mlp_mixer(channels, num_classes):
+    mlp_mixer = timm.create_model("mixer_b16_224.goog_in21k_ft_in1k", pretrained=False, img_size=120,num_classes=num_classes,in_chans=channels)
+    return mlp_mixer
