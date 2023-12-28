e2dbae7866300b607f18e6bd3902ca0d4dbadd0a
s47v <sebastian.voelkers@campus.tu-berlin.de>
2023-12-28 15:31:54 +0100

    Training.py

---
+++ train.py
@@ -0,0 +1,52 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from pathlib import Path
+import time
+import torch
+from utils.pytorch_models import ResNet18
+from models.poolformer import create_poolformer_s12
+from models.ConvMixer import create_convmixer_1024_20
+from models.MLPMixer import create_mlp_mixer
+from utils.clients import GlobalClient
+from utils.pytorch_utils import start_cuda
+
+
+
+def train():
+	c1_path = Path("data/c1_train.csv")
+	c2_path = Path("data/c2_train.csv")
+	c3_path = Path("data/c3_train.csv")
+	if not (c1_path.exists() and c2_path.exists() and c3_path.exists()):
+	    df = pd.read_csv("data/train.csv", header=None)
+
+	    seed = 42
+
+	    df_c1, temp = train_test_split(df, test_size=(2/3), random_state=seed)
+	    df_c2, df_c3 = train_test_split(temp, test_size=0.5, random_state=seed)
+
+	    df_c1.to_csv("data/c1_train.csv", index=False, header=None)
+	    df_c2.to_csv("data/c2_train.csv", index=False, header=None)
+	    df_c3.to_csv("data/c3_train.csv", index=False, header=None)
+	
+	cuda_no = 1
+	batch_size = 128
+	num_workers = 0
+	epochs = 1
+	communication_rounds = 1
+
+	channels = 10
+	num_classes = 19
+	dataset_filter = "serbia"
+	mlp_mixer = create_mlp_mixer(channels, num_classes)
+	global_client_mlp_mixer = GlobalClient(
+	    model=mlp_mixer,
+	    lmdb_path="/faststorage/BigEarthNet_S1_S2/BEN_S1_S2.lmdb",
+	    val_path="data/test.csv",
+	    csv_paths=["data/c1_train.csv", "data/c2_train.csv", "data/c3_train.csv"],
+	)
+	global_mlp_mixer_results, global_mlp_mixer_client_results = global_client_mlp_mixer.train(communication_rounds=communication_rounds, epochs=epochs)
+
+
+
+if __name__ == '__main__':
+    train()
